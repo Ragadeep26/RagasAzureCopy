761b74a760e2bb5057cfe012a089e89636784e14
Ragadeep Bojja <80882218+Ragadeep26@users.noreply.github.com>
2023-04-26 08:55:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import sys
 import streamlit as st
 import numpy as np
 import matplotlib as mpl
+from Reinforcement_helper import main_reinforced_concrete_helper
+
+if __name__ == '__main__':
+  main_reinforced_concrete_helper(st)
