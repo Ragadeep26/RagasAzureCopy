fb645e22e33b7b47e5d23573d3f96599cf614b5a
Ragadeep Bojja <80882218+Ragadeep26@users.noreply.github.com>
2023-04-26 09:07:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import sys
 import streamlit as st
 from Reinforcement_helper import main_reinforced_concrete_helper
-st.set_page_config(page_title='Ragatries', layout="wide", page_icon="⚙️")
+
+st.set_page_config(page_title='Piles & panels', layout="wide", page_icon="⚙️")
+
 
 if __name__ == '__main__':
-  # Sidebar
-  st.sidebar.markdown('# Form selection')
-  select_options = ['concretehelp' ]
+    # Sidebar
+    st.sidebar.markdown('# Form selection')
+    select_options = ['Concrete_helper']
+    select_event = st.sidebar.selectbox('Select one of the forms', select_options, key='selected_form', index=0)
 
-  select_event = st.sidebar.selectbox('Select one of the forms', select_options, key='selected_form', index=0)
-  if select_event == 'concretehelp'
-    main_reinforced_concrete_helper(st)
-  else:
-    pass
+    if select_event == 'Concrete_helper':
+        main_reinforced_concrete_helper(st)
